d4a825ee75cdf17efdbfecd6c697d47122e03331
olivier <o.gozlan@ajc-ingenierie.fr>
2022-02-11 12:24:24 +0100

    entity poste

---
+++ olivier/formationSpringJpa/src/main/java/formationJpa/model/Poste.java
@@ -0,0 +1,22 @@
+package formationJpa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sal")
+public class Poste {
+	@Id
+	@Column(name = "job_id", length = 50)
+	private String code;
+	@Column(name = "job_name", length = 100)
+	private String libelle;
+	@Column(name = "job_min_sal")
+	private double salaireMin;
+	@Column(name = "job_max_sal")
+	private double salaireMax;
+	
+	//a vous de generer le code manquant
+}
