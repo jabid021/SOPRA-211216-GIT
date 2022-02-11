543b962e8925df7f7f1e6eabf31cddb345296a7a
olivier <o.gozlan@ajc-ingenierie.fr>
2022-02-11 12:25:19 +0100

    modif table name

--- olivier/formationSpringJpa/src/main/java/formationJpa/model/Poste.java
+++ olivier/formationSpringJpa/src/main/java/formationJpa/model/Poste.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "sal")
+@Table(name = "job")
 public class Poste {
 	@Id
 	@Column(name = "job_id", length = 50)
